a1ff04a8f8efaeb021f5a25f4698b60ef53ac3e5
gokkuu100 <wkurts247@gmail.com>
2024-02-13 11:26:44 +0300

    jwt authentication

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,24 +1,23 @@
 const express = require('express')
 const router = express.Router();
-const jwt = require('jsonwebtoken')
 const { UserController } = require('../controllers/controller')
 require("dotenv").config()
 
-const authenticateToken = (req, res, next) => {
-    const token = req.header('Authorization')
+const passport = require('passport');
+const jwtStrategy = require('passport-jwt').Strategy;
+const { ExtractJwt } = require('passport-jwt');
 
-    if (!token) {
-        return res.status(401).json({ message: 'Unauthorized'})
-    }
-    jwt.verify(token, process.env.SECRET_KEY, (err, user) => {
-        if (err) {
-            return res.sendStatus(403).json({ message: 'Forbidden'})
-        } else {
-            req.user = user;
-            next()
-        }
-    })
-}
+// Configure Passport JWT Strategy
+const jwtOptions = {
+    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    secretOrKey: process.env.SECRET_KEY, 
+  };
+  
+  passport.use(new jwtStrategy(jwtOptions, (jwtPayload, done) => {
+    return done(null, { userId: jwtPayload.userId });
+  }));
+  
+
 
 router.post( '/registerMentor', UserController.createMentor )
 router.post('/registerStudent', UserController.createStudent)
@@ -27,7 +26,8 @@ router.post('/assessments', UserController.createAssessment)
 router.post('/questions/:mentorId/:assessmentId', UserController.createQuestions)
 router.post('/answers', UserController.submitAnswer)
 router.post('/sendInvite', UserController.sendInvite)
-router.post('/acceptInvitation/:inviteId',UserController.respondToInvite)
+router.post('/acceptInvitation/:invite',UserController.respondToInvite)
 
-router.get('/notifications/:studentId', UserController.getNotification)
+router.get('/notifications/:studentId', passport.authenticate('jwt', { session: false }) ,UserController.getNotification)
+
 module.exports = {router}
