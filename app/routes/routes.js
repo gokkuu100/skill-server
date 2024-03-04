9dcbd9a86622b75c4460701cf55ed5c53763d6e6
gokkuu100 <wkurts247@gmail.com>
2024-03-04 21:15:39 +0300

    authentication

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -22,19 +22,19 @@ const jwtOptions = {
 router.post( '/registerMentor', UserController.createMentor )
 router.post('/registerStudent', UserController.createStudent)
 router.post('/login', UserController.login)
-router.post('/assessments', UserController.createAssessment)
-router.post('/questions/:mentorId/:assessmentId', UserController.createQuestions)
-router.post('/answers', UserController.submitAnswer)
-router.post('/sendInvite', UserController.sendInvite)
-router.post('/acceptInvitation/:inviteId',UserController.respondToInvite)
+router.post('/assessments', passport.authenticate('jwt', {session: false}), UserController.createAssessment)
+router.post('/questions/:mentorId/:assessmentId', passport.authenticate('jwt', {session: false}), UserController.createQuestions)
+router.post('/answers', passport.authenticate('jwt', {session: false}), UserController.submitAnswer)
+router.post('/sendInvite', passport.authenticate('jwt', {session: false}), UserController.sendInvite)
+router.post('/acceptInvitation/:inviteId', passport.authenticate('jwt', {session: false}), UserController.respondToInvite)
 
 router.get('/notifications/:studentId', passport.authenticate('jwt', { session: false }), UserController.getNotification)
 router.get('/student/:studentId', passport.authenticate('jwt', {session: false}), UserController.getAssessmentDetails);
-router.get('/questions/:assessmentId', UserController.getAssessmentQuestions)
-router.get('/grades/:studentId/', UserController.getStudentGrades)
-router.get('/students', UserController.getAllStudents)
-router.get('/assessments/:mentorId', UserController.getAssessmentByMentor)
-router.get('/allgrades/:mentorId', UserController.getGradesByMentor)
+router.get('/questions/:assessmentId', passport.authenticate('jwt', {session: false}), UserController.getAssessmentQuestions)
+router.get('/grades/:studentId/', passport.authenticate('jwt', {session: false}), UserController.getStudentGrades)
+router.get('/students', passport.authenticate('jwt', {session: false}), UserController.getAllStudents)
+router.get('/assessments/:mentorId', passport.authenticate('jwt', {session: false}), UserController.getAssessmentByMentor)
+router.get('/allgrades/:mentorId', passport.authenticate('jwt', {session: false}), UserController.getGradesByMentor)
 
 
 
